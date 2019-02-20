608686b8ef8f32b4dfb4e60eda0298c65a95c858
marinatuji <mtujimura@gmail.com>
2019-02-20 14:38:58 -0300

    Atualizado até versão 0.0.2

--- twitter.js
+++ twitter.js
@@ -1,9 +1,43 @@
-function captionMessage(event, form) {
+function captureMessage(event, form) {
 	event.preventDefault();
 	let messageWrited = form.messageWrited.value;
-	document.getElementById('display').innerHTML = messageWrited;
+	document.getElementById('display').textContent = messageWrited;
+	disableButton();
+	cleanContent();
+	return messageWrited;
 }
 
-function countCharacteres(form){
-	// event.composed;
+function cleanContent() {
+	document.getElementById('input-textarea').value = '';
 }
+
+function disableButton() {
+	document.getElementsByName('submitTweet')[0].disabled = true;
+}
+
+function enableButton() {
+	document.getElementsByName('submitTweet')[0].disabled = false;
+}
+
+function createTweets() {
+ 	document.getElementsByName('submitTweet')[0].disabled = false;
+	let i = 0;
+	let tweet = document.createElement('p');
+	document.getElementsByTagName('p')[i];
+	i ++;
+}
+
+function countChars(event) {
+	const messageChars = event.value.length ;//tamnho da minha msg
+	const messageSize = 140 - messageChars;
+	document.getElementById('regressive-counter').value = messageSize;
+	return messageSize;
+}
+
+function counterBehavior(event) {
+	if(event.value == 0) {
+		disableButton();
+	}
+		enableButton();
+}
+//tentar fazer a opção 2 que é com o onsubmit e disabled e uma função pra cada coisa
