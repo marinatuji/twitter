29611771b1389148f12b1bc16256dbdb6739df33
marinatuji <mtujimura@gmail.com>
2019-02-21 01:37:12 -0300

    atualizado até versão 0.0.3

--- twitter.js
+++ twitter.js
@@ -4,6 +4,7 @@ function captureMessage(event, form) {
 	document.getElementById('display').textContent = messageWrited;
 	disableButton();
 	cleanContent();
+	createTweets();
 	return messageWrited;
 }
 
@@ -20,24 +21,32 @@ function enableButton() {
 }
 
 function createTweets() {
- 	document.getElementsByName('submitTweet')[0].disabled = false;
+	document.getElementsByName('submitTweet')[0].disabled = false;
 	let i = 0;
 	let tweet = document.createElement('p');
 	document.getElementsByTagName('p')[i];
-	i ++;
+	i++;
 }
 
 function countChars(event) {
-	const messageChars = event.value.length ;//tamnho da minha msg
+	const messageChars = event.value.length;//tamnho da minha msg
 	const messageSize = 140 - messageChars;
-	document.getElementById('regressive-counter').value = messageSize;
+	const counterElement = document.getElementById('regressive-counter');
+	counterElement.value = messageSize;
+	counterBehavior(counterElement);
 	return messageSize;
 }
 
-function counterBehavior(event) {
-	if(event.value == 0) {
+function counterBehavior(element) {
+	const counterValue = parseInt(element.value);
+	if (counterValue < 0) {
 		disableButton();
+		element.style.color = 'grey';
+	} else if (counterValue > 10 && counterValue <= 20) {
+		element.style.color = 'orange';
+	} else if (counterValue > 0 && counterValue <= 10) {
+		element.style.color = 'red';
+	} else if (counterValue > 20 && counterValue <= 140) {
+		element.style.color = 'black';
 	}
-		enableButton();
 }
-//tentar fazer a opção 2 que é com o onsubmit e disabled e uma função pra cada coisa
